f02fe09c74c56f0c9d27885d0d1702d7aac262b2
SquawkyToucan <league@WTS13.attlocal.net>
2018-02-23 18:16:22 -0800

    Commit

--- src/oncogene/OncogeneDetector.java
+++ src/oncogene/OncogeneDetector.java
@@ -3,9 +3,12 @@ package oncogene;
 import java.util.ArrayList;
 
 public class OncogeneDetector {
-
+	/*
+	 * Put letters into linked list
+	 * At each letter, find the next and if needed next after and so on
+	 * if(isNext) and if they are all true then it is a cancer sequence
+	 */
 	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String string) {
-		
 		return false;
 	}
 
