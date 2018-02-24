bd4b01a280123a6e1d3c2ca8bb19a9c0f5d9174f
SquawkyToucan <league@WTS13.attlocal.net>
2018-02-23 18:24:02 -0800

    Beginning Oncogenes

--- src/oncogene/OncogeneDetector.java
+++ src/oncogene/OncogeneDetector.java
@@ -9,6 +9,13 @@ public class OncogeneDetector {
 	 * if(isNext) and if they are all true then it is a cancer sequence
 	 */
 	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String string) {
+		/**
+		 * Have an integer for amount  of cancer with trait and amount of healthy with trait
+		 * Go through each, adding one to an int if it has the trait
+		 * When done, divide, and check to see if greater
+		 * If cancer > normal, it is cancer, else, not cancer
+		 * Return true if it is cancer
+		 */
 		return false;
 	}
 
