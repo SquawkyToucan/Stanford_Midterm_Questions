8dc2e09cc81dc2bb4afb4fd86335215a43fc66bd
League <league@WTS9.attlocal.net>
2018-03-02 18:07:37 -0800

    Finished

--- src/animal_hipster/AnimalHipster.java
+++ src/animal_hipster/AnimalHipster.java
@@ -8,7 +8,32 @@ public class AnimalHipster {
 
 	public AbstractCollection<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network,
 			HashMap<String, String> favoriteAnimals) {
-		return null;
+		// Go through each person
+		// Check all friends
+		// If friend has same, disqualify
+		// If gone through all friends and none has same animal, add to collection
+		AbstractCollection<String> hipsters = new ArrayList<>();
+		String name;
+		System.out.println("beginnning a test");
+		for (int i = 0; i < network.keySet().size(); i++) {
+			boolean isHipster = true;
+			name = (String) network.keySet().toArray()[i];
+			System.out.println("testing " + name);
+			for(int j = 0; j < network.get(name).size(); j++) {
+				String nameBeingChecked = network.get(name).get(j);
+				System.out.println("checking " + name + " against " + nameBeingChecked);
+				if(favoriteAnimals.get(nameBeingChecked).equals(favoriteAnimals.get(name))) {
+					// Remove them, as it is impossible for them to be hipsters
+					isHipster = false;
+					System.out.println(name + " has been removed from contention along with " + nameBeingChecked);
+				}
+			}
+			if(isHipster) {
+				hipsters.add(name);
+			}
+		}
+		
+		return hipsters;
 	}
 
 }
